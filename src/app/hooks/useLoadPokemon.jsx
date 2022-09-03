cf42500f7d7f64ad6137433b92d1e79ec4e34a25
George Roubie <george.roubie@gmail.com>
2022-09-03 19:09:05 +0300

    Use async await in load pokemon hook

--- src/app/hooks/useLoadPokemon.jsx
+++ src/app/hooks/useLoadPokemon.jsx
@@ -6,21 +6,22 @@ import { AppContext } from '../state/Context';
 function useLoadPokemon() {
   const { setPokemon, setLoading } = useContext(AppContext);
 
-  function loadPokemon(url, callback) {
+  async function loadPokemon(url, callback) {
     setLoading(true);
-    getPokemon(url).then(({ data: pokemon }) => {
-      getPokemonSpecies(pokemon.species.url).then(({ data: species }) => {
-        getEvolutionChain(species.evolution_chain.url).then(({ data: { chain } }) => {
-          setPokemon({
-            ...pokemon,
-            ...species,
-            evolutions: getEvolutions(chain),
-          });
-          setLoading(false);
-          callback?.();
-        });
-      });
+
+    const { data: pokemon } = await getPokemon(url);
+    const { data: species } = await getPokemonSpecies(pokemon.species.url);
+    const {
+      data: { chain },
+    } = await getEvolutionChain(species.evolution_chain.url);
+
+    setPokemon({
+      ...pokemon,
+      ...species,
+      evolutions: getEvolutions(chain),
     });
+    setLoading(false);
+    callback?.();
   }
 
   return loadPokemon;
