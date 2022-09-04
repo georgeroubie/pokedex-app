e72a6a43525cae315acd4d69e6b6c259f03191e4
George Roubie <george.roubie@gmail.com>
2022-09-04 12:35:45 +0300

    Rename Search component to SearchPokemon

---
+++ src/app/components/pokemon/SearchPokemon.jsx
@@ -0,0 +1,53 @@
+import { useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { AppContext } from '../../state/Context';
+import _PokemonNames from './PokemonNames';
+
+const Wrapper = styled.div``;
+
+const Input = styled.input`
+  width: 100%;
+  border: 0;
+  border-radius: 0;
+  outline: none;
+  background-color: ${({ theme: { colors } }) => colors.backgroundPrimary};
+  color: ${({ theme: { colors } }) => colors.textPrimary};
+  padding: ${({ theme: { spacing } }) => `${spacing.small} ${spacing.normal}`};
+  border-bottom: ${({ theme: { border, colors } }) => `${border.size} solid ${colors.borderPrimary}`};
+`;
+
+const PokemonNames = styled(_PokemonNames)`
+  padding: ${({ theme: { spacing } }) => spacing.normal};
+`;
+
+const SearchPokemon = () => {
+  const {
+    state: { loading, pokemonNames },
+  } = useContext(AppContext);
+  const [pokemonName, setPokemonName] = useState('');
+  const [filteredPokemonNames, setFilteredPokemonNames] = useState([]);
+
+  useEffect(() => {
+    if (pokemonName) {
+      const filteredPokemons = pokemonNames.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(pokemonName.toLowerCase()),
+      );
+      setFilteredPokemonNames(filteredPokemons);
+    } else {
+      setFilteredPokemonNames([]);
+    }
+  }, [pokemonName, pokemonNames]);
+
+  function handleChange({ target }) {
+    setPokemonName(target.value);
+  }
+
+  return (
+    <Wrapper>
+      <Input type="text" placeholder="Pokemon name" value={pokemonName} onChange={handleChange} disabled={loading} />
+      <PokemonNames pokemons={filteredPokemonNames} onClick={() => setPokemonName('')} />
+    </Wrapper>
+  );
+};
+
+export default SearchPokemon;
--- src/app/pages/home/index.jsx
+++ src/app/pages/home/index.jsx
@@ -2,12 +2,12 @@ import PageWrapper from '../../components/layout/PageWrapper';
 import PokedexBottom from '../../components/layout/PokedexBottom';
 import PokedexTop from '../../components/layout/PokedexTop';
 import Pokemon from '../../components/pokemon/Pokemon';
-import Search from '../../components/search';
+import SearchPokemon from '../../components/pokemon/SearchPokemon';
 
 const Home = () => (
   <PageWrapper>
     <PokedexTop>
-      <Search />
+      <SearchPokemon />
     </PokedexTop>
     <PokedexBottom>
       <Pokemon />
