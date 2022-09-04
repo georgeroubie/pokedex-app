7c9a823413f98086a00cb805c995243b31a0dbb5
George Roubie <george.roubie@gmail.com>
2022-09-04 13:44:24 +0300

    Add basic pwa configuration

--- src/app/theme/styles/base.js
+++ src/app/theme/styles/base.js
@@ -9,6 +9,12 @@ const base = css`
 
   html,
   body {
+    width: 100vw;
+    height: 100vh;
+    overflow-y: auto;
+    overflow-x: hidden;
+    user-select: none;
+    -webkit-tap-highlight-color: transparent;
     background-color: ${({ theme: { colors } }) => colors.backgroundPrimary};
   }
 `;
