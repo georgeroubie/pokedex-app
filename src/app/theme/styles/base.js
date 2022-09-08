1c56ed9316c3197e616ad51312565fb675f52ea2
George Roubie <george.roubie@gmail.com>
2022-09-08 22:34:15 +0300

    Remove height from body

--- src/app/theme/styles/base.js
+++ src/app/theme/styles/base.js
@@ -9,10 +9,6 @@ const base = css`
 
   html,
   body {
-    width: 100vw;
-    height: 100vh;
-    overflow-y: auto;
-    overflow-x: hidden;
     user-select: none;
     -webkit-tap-highlight-color: transparent;
     background-color: ${({ theme: { colors } }) => colors.backgroundPrimary};
