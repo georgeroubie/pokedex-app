9dc24ca831b9edfaf1c5a6d2e81944a49576d6e2
George Roubie <george.roubie@gmail.com>
2022-09-13 14:00:00 +0100

    Create a mini game page

--- src/app/pages/mini-games/index.jsx
+++ src/app/pages/mini-games/index.jsx
@@ -1,3 +1,4 @@
+import { NavLink as _NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import PageWrapper from '../../components/layout/PageWrapper';
 import PokedexBottom from '../../components/layout/PokedexBottom';
@@ -9,15 +10,42 @@ const TopWrapper = styled.div`
   padding: ${({ theme: { spacing } }) => spacing.normal};
 `;
 
+const NavLink = styled(_NavLink)`
+  display: block;
+  width: 100%;
+  text-align: center;
+  text-decoration: none;
+  padding: ${({ theme: { spacing } }) => spacing.xsmall};
+  margin-bottom: ${({ theme: { spacing } }) => spacing.normal};
+  border-radius: ${({ theme: { border } }) => border.radius};
+  background-color: ${({ theme: { colors } }) => colors.textPrimary};
+  color: ${({ theme: { colors } }) => colors.backgroundPrimary};
+  font-size: ${({ theme: { fontSize } }) => fontSize.normal};
+  line-height: ${({ theme: { lineHeight } }) => lineHeight.normal};
+`;
+
+const availableGames = [
+  {
+    name: `Who's that pokemon`,
+    url: '/mini-games/find-pokemon-name',
+  },
+];
+
 const MiniGames = () => (
   <PageWrapper>
     <PokedexTop>
       <TopWrapper>
-        <Subtitle>Mini Games</Subtitle>
+        <Subtitle>Mini-games</Subtitle>
+        <Description>Let's have some fun!</Description>
       </TopWrapper>
     </PokedexTop>
     <PokedexBottom>
-      <Description>Coming soon!</Description>
+      <Subtitle>Available games now</Subtitle>
+      {availableGames.map(({ name, url }) => (
+        <NavLink key={name} to={url}>
+          {name}
+        </NavLink>
+      ))}
     </PokedexBottom>
   </PageWrapper>
 );
