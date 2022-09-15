9dc1c92a97ce9fce2267426eb48193ea47c70d2e
George Roubie <george.roubie@gmail.com>
2022-09-15 22:08:52 +0300

    Update index.jsx

--- src/app/pages/mini-games/find-pokemon-name/game/index.jsx
+++ src/app/pages/mini-games/find-pokemon-name/game/index.jsx
@@ -1,3 +1,38 @@
-const FindPokemonNameGame = () => <div>index</div>;
+import { useContext, useMemo } from 'react';
+import PageWrapper from '../../../../components/layout/PageWrapper';
+import { AppContext } from '../../../../state/Context';
+import FindPokemonNameGameWrapper from './FindPokemonNameGameWrapper';
+
+const FindPokemonNameGame = () => {
+  const { state } = useContext(AppContext);
+  const { pokemonNames } = state;
+
+  const availablePokemonNames = useMemo(() => {
+    if (!pokemonNames?.length) {
+      return null;
+    }
+
+    return pokemonNames.filter(({ name }) => {
+      let includePokemon = true;
+      const invalidCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'];
+      invalidCharacters.forEach((character) => {
+        if (name.includes(character)) {
+          includePokemon = false;
+        }
+      });
+      return includePokemon;
+    });
+  }, [pokemonNames]);
+
+  if (!availablePokemonNames) {
+    return null;
+  }
+
+  return (
+    <PageWrapper>
+      <FindPokemonNameGameWrapper pokemonNames={availablePokemonNames} />
+    </PageWrapper>
+  );
+};
 
 export default FindPokemonNameGame;
