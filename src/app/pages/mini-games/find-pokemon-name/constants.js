ab0a7ae3b4ab267a544495ecb3755a83d3b15c7f
George Roubie <george.roubie@gmail.com>
2022-09-15 23:38:33 +0300

    Create constants.js

---
+++ src/app/pages/mini-games/find-pokemon-name/constants.js
@@ -0,0 +1,34 @@
+const LIVES = 3;
+
+const LETTERS = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+const INVALID_CHARACTERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'];
+
+export { LIVES, LETTERS, INVALID_CHARACTERS };
