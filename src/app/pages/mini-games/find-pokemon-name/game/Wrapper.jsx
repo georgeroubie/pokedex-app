fbc8884fbb98a81a5898486ad8474507fb979bbc
George Roubie <george.roubie@gmail.com>
2022-09-17 19:47:04 +0300

    Update Wrapper.jsx

--- src/app/pages/mini-games/find-pokemon-name/game/Wrapper.jsx
+++ src/app/pages/mini-games/find-pokemon-name/game/Wrapper.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import PokedexBottom from '../../../../components/layout/PokedexBottom';
 import PokedexTop from '../../../../components/layout/PokedexTop';
 import { randomNumber } from '../../../../helpers/generators';
@@ -9,37 +9,33 @@ import BlurredPokemon from './BlurredPokemon';
 import SelectLetter from './SelectLetter';
 
 const FindPokemonNameGameWrapper = ({ pokemonNames }) => {
-  const [activeGame, setActiveGame] = useState();
+  const activeGame = useRef(null);
 
   const startGame = useCallback(async () => {
     const randomIndex = randomNumber(0, pokemonNames.length);
     const randomPokemon = pokemonNames[randomIndex];
     const pokemon = await getPokemonData(randomPokemon.url);
 
-    setActiveGame(() => ({
+    activeGame.current = {
       lives: LIVES,
       name: pokemon.name,
       image: pokemon.sprites.front,
       nameArray: pokemon.name.split(''),
       foundNameArray: pokemon.name.split('').map(() => ''),
-    }));
+    };
   }, [pokemonNames]);
 
   useEffect(() => {
     startGame();
   }, [startGame]);
 
-  if (!activeGame) {
-    return null;
-  }
-
   return (
     <>
       <PokedexTop>
-        <BlurredPokemon game={activeGame} />
+        <BlurredPokemon game={activeGame.current} />
       </PokedexTop>
       <PokedexBottom>
-        <SelectLetter game={activeGame} setGame={setActiveGame} />
+        <SelectLetter game={activeGame.current} setGame={() => {}} />
       </PokedexBottom>
     </>
   );
