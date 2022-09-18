77d65cd76a4b54ee4856b92252fe7690eb25f277
George Roubie <george.roubie@gmail.com>
2022-09-18 19:56:47 +0300

    Create reducer.js

---
+++ src/app/pages/mini-games/find-pokemon-name/state/reducer.js
@@ -0,0 +1,34 @@
+import * as actionTypes from './actions';
+
+const findPokemonNameReducer = (state, { type, value }) => {
+  switch (type) {
+    case actionTypes.UPDATE_LOADING:
+      return {
+        ...state,
+        loading: value,
+      };
+
+    case actionTypes.UPDATE_LIVES:
+      return {
+        ...state,
+        lives: value,
+      };
+    case actionTypes.UPDATE_POKEMON:
+      return {
+        ...state,
+        pokemon: {
+          ...state.pokemon,
+          ...value,
+        },
+      };
+    case actionTypes.UPDATE_PLAYER_FOUNDS:
+      return {
+        ...state,
+        playerFounds: [...state.playerFounds, ...value],
+      };
+    default:
+      return state;
+  }
+};
+
+export { findPokemonNameReducer };
