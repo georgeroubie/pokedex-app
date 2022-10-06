6e771c9a53c50fd458a1820096cd6d811fa8db5a
George Roubie <george.roubie@gmail.com>
2022-10-06 22:50:11 +0300

    Update Wrapper.jsx

--- src/app/pages/mini-games/find-pokemon-name/game/Wrapper.jsx
+++ src/app/pages/mini-games/find-pokemon-name/game/Wrapper.jsx
@@ -1,31 +1,13 @@
-import { useCallback, useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
+import PageWrapper from '../../../../components/layout/PageWrapper';
 import PokedexBottom from '../../../../components/layout/PokedexBottom';
 import PokedexTop from '../../../../components/layout/PokedexTop';
-import { getPokemonData, transformPokemonIdToPokemonApiUrl } from '../../../../helpers/requests';
-import useGetRandomPokemonId from '../hooks/useGetRandomPokemonId';
 import { FindPokemonNameContext } from '../state/Context';
 import BlurredPokemon from './BlurredPokemon';
 import SelectLetter from './SelectLetter';
 
 const FindPokemonNameGameWrapper = () => {
-  const randomPokemonId = useGetRandomPokemonId();
-  const { setPokemon, setPlayerFounds, setLoading } = useContext(FindPokemonNameContext);
-
-  const startGame = useCallback(async () => {
-    const apiUrl = transformPokemonIdToPokemonApiUrl(randomPokemonId);
-    const pokemon = await getPokemonData(apiUrl);
-    console.log(pokemon);
-
-    setPokemon({
-      name: pokemon.name,
-      image: pokemon.sprites.front,
-      nameArray: pokemon.name.split(''),
-    });
-
-    setPlayerFounds(pokemon.name.split('').map(() => ''));
-
-    setLoading(false);
-  }, [randomPokemonId, setPokemon, setPlayerFounds, setLoading]);
+  const { startGame } = useContext(FindPokemonNameContext);
 
   useEffect(() => {
     startGame();
@@ -33,14 +15,14 @@ const FindPokemonNameGameWrapper = () => {
   }, []);
 
   return (
-    <>
+    <PageWrapper>
       <PokedexTop>
         <BlurredPokemon />
       </PokedexTop>
       <PokedexBottom>
         <SelectLetter />
       </PokedexBottom>
-    </>
+    </PageWrapper>
   );
 };
 
