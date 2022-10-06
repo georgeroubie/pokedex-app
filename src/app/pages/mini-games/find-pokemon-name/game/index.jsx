af8fbd48242ec5cf32bb5e5ace796eed3b7d5e8a
George Roubie <george.roubie@gmail.com>
2022-10-06 21:11:28 +0300

    Update index.jsx

--- src/app/pages/mini-games/find-pokemon-name/game/index.jsx
+++ src/app/pages/mini-games/find-pokemon-name/game/index.jsx
@@ -1,47 +1,13 @@
-import { useCallback, useEffect, useState } from 'react';
 import PageWrapper from '../../../../components/layout/PageWrapper';
-import PokedexBottom from '../../../../components/layout/PokedexBottom';
-import PokedexTop from '../../../../components/layout/PokedexTop';
-import { getPokemonData, transformPokemonIdToPokemonApiUrl } from '../../../../helpers/requests';
-import { LIVES } from '../constants';
-import useGetRandomPokemonId from '../hooks/useGetRandomPokemonId';
-import BlurredPokemon from './BlurredPokemon';
-import SelectLetter from './SelectLetter';
+import { FindPokemonNameProvider } from '../state/Context';
+import FindPokemonNameGameWrapper from './Wrapper';
 
-const FindPokemonNameGame = () => {
-  const [game, setGame] = useState();
-  const randomPokemonId = useGetRandomPokemonId();
-
-  const startGame = useCallback(async () => {
-    const pokemon = await getPokemonData(transformPokemonIdToPokemonApiUrl(randomPokemonId));
-
-    setGame({
-      lives: LIVES,
-      name: pokemon.name,
-      image: pokemon.sprites.front,
-      nameArray: pokemon.name.split(''),
-      foundNameArray: pokemon.name.split('').map(() => ''),
-    });
-  }, [randomPokemonId]);
-
-  useEffect(() => {
-    startGame();
-  }, [randomPokemonId, startGame]);
-
-  if (!game) {
-    return null;
-  }
-
-  return (
+const FindPokemonNameGame = () => (
+  <FindPokemonNameProvider>
     <PageWrapper>
-      <PokedexTop>
-        <BlurredPokemon game={game} />
-      </PokedexTop>
-      <PokedexBottom>
-        <SelectLetter game={game} setGame={setGame} />
-      </PokedexBottom>
+      <FindPokemonNameGameWrapper />
     </PageWrapper>
-  );
-};
+  </FindPokemonNameProvider>
+);
 
 export default FindPokemonNameGame;
