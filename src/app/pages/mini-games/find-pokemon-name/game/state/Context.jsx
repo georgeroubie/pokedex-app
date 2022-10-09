42883685d9f0e6008a689b37dbd00862b42d9cb9
George Roubie <george.roubie@gmail.com>
2022-10-09 11:38:19 +0300

    Create Context.jsx

---
+++ src/app/pages/mini-games/find-pokemon-name/game/state/Context.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import { createContext } from 'react';
+import { useFindPokemonNameState } from './store';
+
+const FindPokemonNameContext = createContext();
+
+const FindPokemonNameProvider = ({ pokemonNames, children }) => (
+  <FindPokemonNameContext.Provider value={useFindPokemonNameState(pokemonNames)}>
+    {children}
+  </FindPokemonNameContext.Provider>
+);
+
+FindPokemonNameProvider.propTypes = {
+  pokemonNames: PropTypes.arrayOf(PropTypes.shape({})),
+  children: PropTypes.node.isRequired,
+};
+
+export { FindPokemonNameProvider, FindPokemonNameContext };
