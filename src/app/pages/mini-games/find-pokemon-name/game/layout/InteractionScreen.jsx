aacc8dc01613addb9308f75c8e66a976a7c2bf80
George Roubie <george.roubie@gmail.com>
2022-10-09 15:53:27 +0300

    Update InteractionScreen.jsx

--- src/app/pages/mini-games/find-pokemon-name/game/layout/InteractionScreen.jsx
+++ src/app/pages/mini-games/find-pokemon-name/game/layout/InteractionScreen.jsx
@@ -1,5 +1,16 @@
+import { useContext } from 'react';
 import SelectLetter from '../components/SelectLetter';
+import { FindPokemonNameContext } from '../state/Context';
 
-const InteractionScreen = () => <SelectLetter />;
+const InteractionScreen = () => {
+  const { state } = useContext(FindPokemonNameContext);
+  const { pokemon } = state;
+
+  if (!pokemon?.name) {
+    return null;
+  }
+
+  return <SelectLetter />;
+};
 
 export default InteractionScreen;
