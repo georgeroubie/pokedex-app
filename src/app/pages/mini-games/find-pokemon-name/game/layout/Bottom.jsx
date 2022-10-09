15eb71fcefa6f6973310dae516e34c20dc0d4605
George Roubie <george.roubie@gmail.com>
2022-10-09 11:38:07 +0300

    Create Bottom.jsx

---
+++ src/app/pages/mini-games/find-pokemon-name/game/layout/Bottom.jsx
@@ -0,0 +1,10 @@
+import PokedexBottom from '../../../../../components/layout/PokedexBottom';
+import SelectLetter from '../components/SelectLetter';
+
+const Bottom = () => (
+  <PokedexBottom>
+    <SelectLetter />
+  </PokedexBottom>
+);
+
+export default Bottom;
