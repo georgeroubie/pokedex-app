450222129c2c15d729737583061e2d77d109c3fa
George Roubie <george.roubie@gmail.com>
2022-10-09 15:53:15 +0300

    Create PageLoader.jsx

---
+++ src/app/components/layout/PageLoader.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import Description from '../typography/Description';
+import PageWrapper from './PageWrapper';
+import PokedexBottom from './PokedexBottom';
+import PokedexTop from './PokedexTop';
+
+const TopWrapper = styled.div`
+  padding: ${({ theme: { spacing } }) => spacing.normal};
+`;
+
+const PageLoader = () => (
+  <PageWrapper>
+    <PokedexTop>
+      <TopWrapper>
+        <Description>Loading...</Description>
+      </TopWrapper>
+    </PokedexTop>
+    <PokedexBottom></PokedexBottom>
+  </PageWrapper>
+);
+
+export default PageLoader;
