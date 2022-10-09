333d0ed2d5ea89ed27a928b649f2e60cec3c9297
George Roubie <george.roubie@gmail.com>
2022-10-09 11:38:24 +0300

    Create store.js

---
+++ src/app/pages/mini-games/find-pokemon-name/game/state/store.js
@@ -0,0 +1,90 @@
+import { useReducer } from 'react';
+import { randomNumber } from '../../../../../helpers/generators';
+import { getPokemonData } from '../../../../../helpers/requests';
+import { INVALID_CHARACTERS, LIVES } from '../constants';
+import * as actionTypes from './actions';
+import { findPokemonNameReducer } from './reducer';
+
+const useFindPokemonNameState = (pokemonNames) => {
+  const [state, dispatch] = useReducer(findPokemonNameReducer, {
+    loading: true,
+    lives: LIVES,
+    score: 0,
+    pokemon: {
+      name: '',
+      image: '',
+      nameArray: [],
+    },
+    pokemonNames: pokemonNames,
+    playerFounds: [],
+    gameStatus: 'ongoing',
+  });
+
+  function setState(type, value) {
+    dispatch({ type, value });
+  }
+
+  function setLoading(value) {
+    setState(actionTypes.UPDATE_LOADING, value);
+  }
+
+  function setScore(value) {
+    setState(actionTypes.UPDATE_SCORE, value);
+  }
+
+  function setLives(value) {
+    setState(actionTypes.UPDATE_LIVES, value);
+  }
+
+  function setPokemon(value) {
+    setState(actionTypes.UPDATE_POKEMON, value);
+  }
+
+  function setPlayerFounds(value) {
+    setState(actionTypes.UPDATE_PLAYER_FOUNDS, value);
+  }
+
+  function setGameStatus(value) {
+    setState(actionTypes.UPDATE_GAME_STATUS, value);
+  }
+
+  async function startGame() {
+    setLoading(true);
+    const availablePokemons = pokemonNames.filter(({ name }) => {
+      let includePokemon = true;
+      INVALID_CHARACTERS.forEach((character) => {
+        if (name.includes(character)) {
+          includePokemon = false;
+        }
+      });
+      return includePokemon;
+    });
+
+    const randomIndex = randomNumber(0, availablePokemons.length - 1);
+    const pokemon = await getPokemonData(availablePokemons[randomIndex].url);
+    console.log(pokemon.name);
+
+    setGameStatus('ongoing');
+    setLives(LIVES);
+    setPokemon({
+      name: pokemon.name,
+      image: pokemon.sprites.front,
+      nameArray: pokemon.name.split(''),
+    });
+    setPlayerFounds(pokemon.name.split('').map(() => ''));
+    setLoading(false);
+  }
+
+  return {
+    state,
+    setLoading,
+    setScore,
+    setLives,
+    setPokemon,
+    setPlayerFounds,
+    setGameStatus,
+    startGame,
+  };
+};
+
+export { useFindPokemonNameState };
