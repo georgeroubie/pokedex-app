aa303bf8376870660e5012439d4fce6c8324f561
George Roubie <george.roubie@gmail.com>
2022-10-09 11:38:09 +0300

    Create Top.jsx

---
+++ src/app/pages/mini-games/find-pokemon-name/game/layout/Top.jsx
@@ -0,0 +1,10 @@
+import PokedexTop from '../../../../../components/layout/PokedexTop';
+import BlurredPokemon from '../components/BlurredPokemon';
+
+const Top = () => (
+  <PokedexTop>
+    <BlurredPokemon />
+  </PokedexTop>
+);
+
+export default Top;
