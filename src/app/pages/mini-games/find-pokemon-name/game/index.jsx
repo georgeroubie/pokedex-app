38db6223f01263d1b1b1155f27ffb5d92c0de88e
George Roubie <george.roubie@gmail.com>
2022-10-11 22:39:05 +0300

    Update index.jsx

--- src/app/pages/mini-games/find-pokemon-name/game/index.jsx
+++ src/app/pages/mini-games/find-pokemon-name/game/index.jsx
@@ -1,14 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { AppContext } from '../../../../state/Context';
 import GameWrapper from './layout/Wrapper';
 import { FindPokemonNameProvider } from './state/Context';
 
 const FindPokemonNameGame = () => {
+  const [searchParams] = useSearchParams();
   const { state } = useContext(AppContext);
   const { pokemonNames } = state;
 
+  const pokemons = useMemo(() => {
+    if (searchParams.get('only-original-pokemon') === 'yes') {
+      return pokemonNames.slice(0, 151);
+    }
+    return pokemonNames;
+  }, [pokemonNames, searchParams]);
+
   return (
-    <FindPokemonNameProvider pokemonNames={pokemonNames}>
+    <FindPokemonNameProvider pokemonNames={pokemons}>
       <GameWrapper />
     </FindPokemonNameProvider>
   );
