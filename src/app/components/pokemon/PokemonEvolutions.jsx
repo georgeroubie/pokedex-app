d46d3d432d3f817a44fc77ec6588d14c2940268e
George Roubie <george.roubie@gmail.com>
2022-10-13 20:20:10 +0300

    Fix evolution pokemon url bug

--- src/app/components/pokemon/PokemonEvolutions.jsx
+++ src/app/components/pokemon/PokemonEvolutions.jsx
@@ -15,16 +15,40 @@ const PokemonEvolutions = () => {
   const { state } = useContext(AppContext);
   const { pokemon, pokemonNames } = state;
 
-  const getPokemonUrl = useCallback((name) => pokemonNames.find((p) => p.name === name)?.url, [pokemonNames]);
+  const getPokemonUrl = useCallback(
+    (name) => {
+      if (name) {
+        const fixedPokemon = pokemonNames.find((p) => p.name === name);
+        if (fixedPokemon && fixedPokemon?.url) {
+          return fixedPokemon.url;
+        }
+
+        const relevantPokemon = pokemonNames.find((p) => p.name.includes(name));
+        if (relevantPokemon && relevantPokemon?.url) {
+          return relevantPokemon.url;
+        }
+      }
+    },
+    [pokemonNames],
+  );
 
   const evolvesTo = useMemo(
     () =>
       pokemon?.evolutions?.reduce((acc, evolution) => {
         if (evolution.pokemon.name === pokemon.name) {
-          const allEvolutions = evolution.evolvesTo.map((e) => ({
-            ...e,
-            url: getPokemonUrl(e.name),
-          }));
+          const allEvolutions = evolution.evolvesTo.reduce((allEvolutionsAcc, e) => {
+            const url = getPokemonUrl(e.name);
+            if (url) {
+              return [
+                ...allEvolutionsAcc,
+                {
+                  ...e,
+                  url,
+                },
+              ];
+            }
+            return allEvolutionsAcc;
+          }, []);
           return [...acc, ...allEvolutions];
         }
         return acc;
@@ -34,38 +58,34 @@ const PokemonEvolutions = () => {
 
   const evolvesFrom = useMemo(() => {
     const name = pokemon.evolvesFromSpecies;
-    if (name) {
+    const url = getPokemonUrl(name);
+    if (url) {
       return {
         name,
-        url: getPokemonUrl(name),
+        url,
       };
     }
   }, [getPokemonUrl, pokemon.evolvesFromSpecies]);
 
   if (!pokemon) return null;
 
-  if (!pokemon.evolutions || !pokemon.evolutions.length) {
-    return (
-      <Wrapper>
-        <Subtitle>Evolutions</Subtitle>
-        <Description>No known evolutions</Description>
-      </Wrapper>
-    );
+  if (!evolvesFrom && !evolvesTo?.length) {
+    return null;
   }
 
   return (
     <Wrapper>
       <Subtitle>Evolutions</Subtitle>
-      {evolvesFrom && (
-        <>
-          <Description>Evolves from:</Description>
-          <PokemonNames pokemons={[evolvesFrom]} />
-        </>
-      )}
       {Boolean(evolvesTo?.length) && (
         <>
           <Description>Evolves to:</Description>
           <PokemonNames pokemons={evolvesTo} />
+        </>
+      )}
+      {evolvesFrom && (
+        <>
+          <Description>Evolves from:</Description>
+          <PokemonNames pokemons={[evolvesFrom]} />
         </>
       )}
     </Wrapper>
