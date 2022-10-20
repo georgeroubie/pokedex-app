6f577b843d747bc9f8ba3909833d2a94918801d1
George Roubie <george.roubie@gmail.com>
2022-10-20 13:43:37 +0300

    Update Routing.jsx

--- src/app/routing/Routing.jsx
+++ src/app/routing/Routing.jsx
@@ -14,67 +14,69 @@ const CancelDonation = lazy(() => import('../pages/donation/Cancel'));
 
 const Loader = ({ children }) => <Suspense fallback={<PageLoader />}>{children}</Suspense>;
 
-const Routing = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route
-      path="/mini-games"
-      element={
-        <Loader>
-          <MiniGames />
-        </Loader>
-      }
-    />
-    <Route
-      path="/mini-games/find-pokemon-name"
-      element={
-        <Loader>
-          <FindPokemonName />
-        </Loader>
-      }
-    />
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/mini-games"
+        element={
+          <Loader>
+            <MiniGames />
+          </Loader>
+        }
+      />
+      <Route
+        path="/mini-games/find-pokemon-name"
+        element={
+          <Loader>
+            <FindPokemonName />
+          </Loader>
+        }
+      />
 
-    <Route
-      path="/mini-games/find-pokemon-name/play"
-      element={
-        <Loader>
-          <FindPokemonNameGame />
-        </Loader>
-      }
-    />
-    <Route
-      path="/settings"
-      element={
-        <Loader>
-          <Settings />
-        </Loader>
-      }
-    />
-    <Route
-      path="/complete-donation"
-      element={
-        <Loader>
-          <CompleteDonation />
-        </Loader>
-      }
-    />
-    <Route
-      path="/cancel-donation"
-      element={
-        <Loader>
-          <CancelDonation />
-        </Loader>
-      }
-    />
-    <Route
-      path="*"
-      element={
-        <Loader>
-          <NotFound />
-        </Loader>
-      }
-    />
-  </Routes>
-);
+      <Route
+        path="/mini-games/find-pokemon-name/play"
+        element={
+          <Loader>
+            <FindPokemonNameGame />
+          </Loader>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <Loader>
+            <Settings />
+          </Loader>
+        }
+      />
+      <Route
+        path="/complete-donation"
+        element={
+          <Loader>
+            <CompleteDonation />
+          </Loader>
+        }
+      />
+      <Route
+        path="/cancel-donation"
+        element={
+          <Loader>
+            <CancelDonation />
+          </Loader>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <Loader>
+            <NotFound />
+          </Loader>
+        }
+      />
+    </Routes>
+  );
+};
 
 export default Routing;
