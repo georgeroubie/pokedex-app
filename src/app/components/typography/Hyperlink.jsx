c67edd717fc6e39efcf1f72a8e4c562c3656850b
George Roubie <george.roubie@gmail.com>
2023-02-12 13:02:10 +0200

    Create Hyperlink.jsx

---
+++ src/app/components/typography/Hyperlink.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.a`
+  text-decoration: underline;
+  color: ${({ theme: { colors } }) => colors.textPrimary};
+  font-size: ${({ theme: { fontSize } }) => fontSize.normal};
+  font-weight: ${({ theme: { fontWeight } }) => fontWeight.normal};
+  line-height: ${({ theme: { lineHeight } }) => lineHeight.normal};
+  display: inline-block;
+`;
+
+const Hyperlink = ({ className, href, target, rel, children }) => {
+  return (
+    <Wrapper className={className} href={href} target={target} rel={rel}>
+      {children}
+    </Wrapper>
+  );
+};
+
+Hyperlink.propTypes = {
+  className: PropTypes.string,
+  href: PropTypes.string.isRequired,
+  target: PropTypes.string,
+  rel: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Hyperlink.defaultProps = {
+  className: '',
+  target: null,
+  rel: 'noreferrer',
+};
+
+export default Hyperlink;
